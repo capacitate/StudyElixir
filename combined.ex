2a7cec69017f5d015adacdc8ce8fdef0cdb4214e
junhong <junhong@junhongs-MacBook-Air.local>
2017-10-08 14:24:58 +0900

    first commit

---
+++ combined.ex
@@ -0,0 +1,8 @@
+defmodule Combined do
+	import Convert
+	def height_to_mph(meters) do
+		# Convert.mps_to_mph(Drop.fall_velocity(meters))
+		# Drop.fall_velocity(meters) |> Convert.mps_to_mph
+		mps_to_mph(Drop.fall_velocity(meters))
+	end
+end
---
+++ elixir.ex
@@ -0,0 +1,11 @@
+defmodule M do
+	def main do
+		data_stuff()
+	end
+
+	def data_stuff do
+		my_int = 123
+		IO.puts "Integer #{is_integer(my_int)}"
+	end
+
+end
