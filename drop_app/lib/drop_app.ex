ce01781063d11bb319a8bb892e1d5295524d8198
junhong <junhong@junhongs-MacBook-Air.local>
2017-10-24 20:47:39 +0900

    add genserver

---
+++ drop_app/lib/drop_app.ex
@@ -0,0 +1,19 @@
+defmodule DropApp do
+  use Application
+  @moduledoc """
+  Documentation for DropApp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DropApp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ drop_app/lib/drop_server_sup.ex
@@ -0,0 +1,45 @@
+defmodule DropSup do
+	use Supervisor
+
+	# convenience method for startup
+
+	def start_link do
+		Supervisor.start_link(__MODULE__, [], [{:name, __MODULE__}])
+	end
+
+	# supervisor callback
+
+	def init([]) do
+		child = [worker(DropServer, [], [])]
+		supervise(child, [{:strategy, :one_for_one}, {:max_restarts, 1}, {:max_seconds, 5}])
+	end
+
+	# def handle_call(request, _from, state) do
+	# 	distance = request
+	# 	reply = {:ok, fall_velocity(distance)}
+	# 	new_state = %State{count: state.count + 1}
+	# 	{:reply, reply, new_state}
+	# end
+
+	# def handle_cast(_msg, state) do
+	# 	IO.puts("So far, calculated #{state.count} velocities.")
+	# 	{:noreply, state}
+	# end
+
+	# def handle_info(_info, state) do
+	# 	{:noreply, state}
+	# end
+
+	# def terminate(_reason, _state) do
+	# 	{:ok}
+	# end
+
+	# def code_change(_old_version, state, _extra) do
+	# 	{:ok, state}
+	# end
+
+	# # internal function
+	# def fall_velocity(distance) do
+	# 	:math.sqrt(2 * 9.8 * distance)
+	# end
+end
